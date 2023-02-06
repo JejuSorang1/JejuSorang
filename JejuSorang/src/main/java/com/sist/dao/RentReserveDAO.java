06f911daea3590f3d33b9ffce0de534c1207bb99
cshnv <cshnv@DESKTOP-UKHPN38>
2023-02-06 15:51:27 +0900

    20230206_채_

--- JejuSorang/src/main/java/com/sist/dao/RentReserveDAO.java
+++ JejuSorang/src/main/java/com/sist/dao/RentReserveDAO.java
@@ -2,6 +2,6 @@ package com.sist.dao;
 import java.util.*;
 import java.sql.*;
 import com.sist.vo.*;
-public class Rent_ReserveDAO {
+public class RentReserveDAO {
 
 }
--- JejuSorang/src/main/java/com/sist/model/RentReserveModel.java
+++ JejuSorang/src/main/java/com/sist/model/RentReserveModel.java
@@ -5,18 +5,18 @@ import javax.servlet.http.HttpServletResponse;
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
 import com.sist.dao.RentDAO;
-import com.sist.dao.Rent_ReserveDAO;
+import com.sist.dao.RentReserveDAO;
 import java.util.*;
 import com.sist.vo.*;
 import java.text.*;
 @Controller
-public class Rent_ReserveModel {
+public class RentReserveModel {
 	@RequestMapping("reserve/rent_reserve.do")
 	  public String reserve_main(HttpServletRequest request,HttpServletResponse response)
 	  {
 		  String cno=request.getParameter("car_no");
 		  //데이터 베이스 연결
-		  Rent_ReserveDAO dao=new Rent_ReserveDAO();
+		  RentReserveDAO dao=new RentReserveDAO();
 		  request.setAttribute("main_jsp", "../reserve/rent_reserve.jsp");
 		  CommonsModel.footerData(request);
 		  return "../main/main.jsp";
