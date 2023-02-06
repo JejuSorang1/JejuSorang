aff8d8fc1fd90e0613fb6079c4faf4dbbe51d235
diddytjq19@naver.com <user@DESKTOP-LB4AJE8>
2023-02-06 14:12:24 +0900

    1234

--- JejuSorang/src/main/java/com/sist/model/Rent_ReserveModel.java
+++ JejuSorang/src/main/java/com/sist/model/Rent_ReserveModel.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
+import com.sist.dao.RentDAO;
 import com.sist.dao.Rent_ReserveDAO;
 import java.util.*;
 import com.sist.vo.*;
@@ -13,9 +14,27 @@ public class Rent_ReserveModel {
 	@RequestMapping("reserve/rent_reserve.do")
 	  public String reserve_main(HttpServletRequest request,HttpServletResponse response)
 	  {
+		  String cno=request.getParameter("car_no");
+		  //데이터 베이스 연결
+		  Rent_ReserveDAO dao=new Rent_ReserveDAO();
 		  request.setAttribute("main_jsp", "../reserve/rent_reserve.jsp");
 		  CommonsModel.footerData(request);
 		  return "../main/main.jsp";
 	  }
-	  
+	@RequestMapping("rent/car_detail.do")
+	public String car_detail(HttpServletRequest request,HttpServletResponse response)
+	{
+	   String cno=request.getParameter("car_no");
+	   
+	   //데이터 베이스 연결
+	   RentDAO dao=new RentDAO();
+	   CarVO vo=dao.car_detail(Integer.parseInt(cno));
+	   
+	   //footer
+	   request.setAttribute("vo", vo);
+
+	   request.setAttribute("main_jsp","../rent/car_detail.jsp");
+	   return "../main/main.jsp";
+	   
+	   }
 }
